c9673391561070664527912293faaa17f67e3525
Aashita Noah <65173923+aashitagloria@users.noreply.github.com>
2025-03-16 21:56:41 +0100

    Update NOAH_AashitaGloria_Session1.R

--- NOAH_AashitaGloria_Session1.R
+++ NOAH_AashitaGloria_Session1.R
@@ -68,21 +68,38 @@ mylist2[[2]]
 mylist2[[1]]
 mylist2[[1]][2]
 
+
+# ---------------- EXERCISES --------------------------
 #Exercise 1.1
-#a
+#a) Put the numbers 31 to 60 in a vector named P and in a matrix with 6 rows and 5 columns named Q.
+
 P <- 31:60
 Q <- matrix(P,nrow=6,ncol=5)
 Q
 
-#b
-Qt <- matrix(P,nrow=6,ncol=5,byrow=TRUE)
+#b) Create a matrix Qt where you fill first the first row, then the second, and so on. 
+Qt <- matrix(P,nrow=6,ncol=5,byrow=TRUE) #By default the order is by column, therefore, we put byrow = TRUE explicitly
 Qt
 
 #Exercise 1.2
-#a
-#b
-#c
-#d
+#a) Construct three random normal vectors of 100 length. Use the rnorm() function. Name these vectors x1, x2 and x3.
+x1 <- rnorm(100)
+x2 <- rnorm(100)
+x3 <- rnorm(100)
+
+#b) Make a data frame called t with three columns (called a, b and c) containing respectively x1, x1+x2 and x1+x2+x3.
+t <- data.frame(cbind(x1,x1+x2,x1+x2+x3))
+colnames(t)<-c("a","b","c")
+
+#c) Call the following functions for this data frame: plot(t) and sd(t). 
+#Can you understand the results? (Short comment starting with # is welcome.
+plot(t) #Pair-wise scattert plot was created
+sd(t) #sd did not work, columns in a data frame may contain different data types, and standard deviation can becalculated for numerical data only.
+
+# d) Modify sd(t) to get the expected results about the three variables in data frame t.
+sd(t$a)
+sd(t$b)
+sd(t$c)
 
 #Exercise 1.3
 
